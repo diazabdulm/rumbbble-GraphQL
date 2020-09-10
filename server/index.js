e6ddec19b8f8f8cee4f1042f98f6714e784719e9
Abdul <diazabdulm@gmail.com>
2020-09-10 15:52:13 -0400

    added jsconfig to client for easier imports of components, introduced proxy for react to speak to backend, server now introduces production-ready settings and compression and enforces https

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
-const server = require("express")();
+const express = require("express");
 const passport = require("passport");
 const session = require("cookie-session");
 const mongoose = require("mongoose");
+const compression = require("compression");
+const enforce = require("express-sslify");
 const { graphqlHTTP } = require("express-graphql");
 
+const app = express();
 const schema = require("./schema");
 const authRouter = require("./routes/auth");
 
@@ -18,9 +21,12 @@ mongoose.connect(process.env.MONGO_URI, {
   useFindAndModify: false,
 });
 
-server.use(session({ secret: process.env.COOKIE_SECRET }));
-server.use(passport.initialize());
-server.use(passport.session());
+server.use(
+  session({ secret: process.env.COOKIE_SECRET }),
+  passport.initialize(),
+  passport.session()
+);
+
 server.use("/auth", authRouter);
 server.use(
   "/graphql",
@@ -30,8 +36,14 @@ server.use(
   })
 );
 
-server.get("/", (request, response) => {
-  response.send("nice!");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(compression());
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 server.listen(PORT);
