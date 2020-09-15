fe93f4bf41b2ccb5df4ab39a02fb28d0342cae88
Abdul <diazabdulm@gmail.com>
2020-09-15 01:16:26 -0400

    adding new post automatically refreshes post list

--- client/src/components/PostCreate.js
+++ client/src/components/PostCreate.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 
-import { CREATE_POST } from "actions/postActions";
+import { GET_POSTS, CREATE_POST } from "actions/postActions";
 
 const FORM_FIELDS = [
   { id: 0, name: "title" },
@@ -12,7 +12,9 @@ const FORM_FIELDS = [
 ];
 
 function PostCreate({ history }) {
-  const [createPost] = useMutation(CREATE_POST);
+  const [createPost] = useMutation(CREATE_POST, {
+    refetchQueries: [{ query: GET_POSTS }],
+  });
   const [postDetails, setPostDetails] = useState({});
 
   const handleChange = ({ target: { name, value } }) => {
