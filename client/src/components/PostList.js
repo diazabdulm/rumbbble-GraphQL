4372afe966050ed62da3c1230631ffe33d7ad33a
Abdul <diazabdulm@gmail.com>
2020-10-23 10:59:58 -0400

    began client-side implementation of infinite scrolling

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 
 import PostListItem from "components/PostListItem";
 import { GET_POSTS } from "actions/postActions";
 
 function PostList() {
+  const infiniteLoadingElement = useRef(null);
   const { loading, error, data } = useQuery(GET_POSTS);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      // alert("boogie man");
+    });
+  }, []);
 
   if (loading) return null;
   if (error) throw Error(error.message);
+
+  const handleScroll = (event) => {
+    // alert("scrolled");
+  };
 
   const renderPosts = data.posts.map((post) => (
     <PostListItem key={post.id} {...post} />
   ));
 
-  return <main>{renderPosts}</main>;
+  return (
+    <main>
+      {renderPosts}
+      <div ref={infiniteLoadingElement} />
+    </main>
+  );
 }
 
 export default PostList;
