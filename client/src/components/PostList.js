30d3fe1de90e70184bf477e1fbf291a686e8c668
Abdul <diazabdulm@gmail.com>
2020-10-25 19:18:52 -0400

    memoized loadMorePosts method to prevent child needlessly re-rendering

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useQuery } from "@apollo/client";
 
 import PostListItem from "components/PostListItem";
@@ -8,12 +9,12 @@ import { GET_POSTS } from "actions/postActions";
 function PostList() {
   const { loading, error, data } = useQuery(GET_POSTS);
 
+  const loadMorePosts = useCallback(() => {
+    console.log("loading...");
+  }, []);
+
   if (loading) return null;
   if (error) throw Error(error.message);
-
-  const loadMorePosts = () => {
-    console.log("loading...");
-  };
 
   const renderPosts = data.posts.map((post) => (
     <PostListItem key={post.id} {...post} />
